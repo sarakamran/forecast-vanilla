b328f8e139fcc565e8e984e187a3314a194747a6
Sara Kamran <sara.kamran72@gmail.com>
2023-01-02 23:00:47 +0330

    forecast-js-template

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formatData(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="https://ssl.gstatic.com/onebox/weather/48/sunny.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   celsiusTemp = Math.round(response.data.main.temp);
@@ -81,83 +110,4 @@ let celsiusLinkElement = document.querySelector("#celsius-link");
 celsiusLinkElement.addEventListener("click", showCelsiusTemperature);
 
 search("Tehran");
-
-/*function formatDate(date) {
-  let hours = date.getHours();
-
-  if (hours < 10) {
-    hours = `0${hours}`; //"0".concat(hours);
-  }
-
-  let minutes = date.getMinutes();
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`; //"0".concat(minutes);
-  }
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`; //"".concat(day, " ").concat(hours, ":").concat(minutes);
-}
-
-function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
-}
-function searchCity(city) {
-  var apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
-    .concat(city, "&appid=")
-    .concat(apiKey, "&units=metric");
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  var city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
-
-function searchLocation(position) {
-  var apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat="
-    .concat(position.coords.latitude, "&lon=")
-    .concat(position.coords.longitude, "&appid=")
-    .concat(apiKey, "&units=metric");
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
-
-//search button
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-
-//current location
-var currentLocationBut = document.querySelector("#current-location-button");
-currentLocationBut.addEventListener("click", getCurrentLocation);
-searchCity("New York");
-*/
+displayForecast();
